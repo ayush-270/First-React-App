801266fb6bcac7a1a99b4b7e931784ecdfec3bb3
ayush-270 <ayush27.work@gmail.com>
2025-02-12 20:39:13 +0530

    Add Todo Function Added

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import "./App.css";
 import { Header } from "./MyComponents/Header";
 import { Footer } from "./MyComponents/Footer";
 import { Todos } from "./MyComponents/Todos";
+import { useState } from "react";
+import { AddTodo } from "./MyComponents/AddTodo";
 
 function App() {
-  const onDelete = () => {
-    console.log("I am on delete")
-  }
+  const onDelete = (todo) => {
+    console.log("I am on delete of todo",todo);
+    setTodos(todos.filter((e) => {
+      return e !==  todo;
 
-  let todos = [
+  }))}
+
+  const [todos, setTodos] = useState([
     { sno: 1, title: "Workout", desc: "You have to go to the GYM" },
     { sno: 2, title: "Coding", desc: "You have to practice coding problems" },
     { sno: 3, title: "Project", desc: "You have to continue with our ongoing project" },
     { sno: 4, title: "Relax", desc: "You have to go for a walk." },
-  ];
+  ]);
   return (
     <>
       <Header Title = "My Todos List" searchBar = {true}/>
+      <AddTodo/>
       <Todos todos = {todos} onDelete= {onDelete}/>
       <Footer/>
     </>
--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,7 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export const Footer = () => {
+  let FooterStyle = {
+    position: "absolute",
+    width: "100%",
+    marginTop: "15px",
+    height: "60px"
+  };
   return (
-    <div>Footer Works!!!</div>
-  )
-}
+    <footer className="bg-dark text-light py-3 " style={FooterStyle}>
+      <p className="text-center">Copyright &copy; MyTodoslsit.com</p>
+    </footer>
+  );
+};
